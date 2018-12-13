3054988a283f90ddec7310934ab7f901ba0036e8
Daniel Murphy <danielmurphy02@gmail.com>
2018-12-13 14:57:15 -0800

    it's alive

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
 const path = require('path');
+const StreamDeck = require('elgato-stream-deck');
 
 class ControlDeck {
 	constructor(buttonLayoutPath) {
-		this.buttonDefinitions = require(path.resolve(
+		this.streamDeck = new StreamDeck();
+		this.streamDeck.clearAllKeys();
+
+		const buttonDefinitions = require(path.resolve(
 			path.dirname(require.main.filename),
 			buttonLayoutPath
 		));
+
+		for (let key in buttonDefinitions) {
+			let button_id = parseInt(key.split('_')[1]);
+			this._initButton(button_id, buttonDefinitions[key]);
+		}
+	}
+
+	_initButton(buttonId, buttonData) {
+		console.log('initiating button ${buttonId}');
+
+		let tool = require(path.resolve(
+			path.dirname(require.main.filename),
+			'node_modules',
+			buttonData.tool
+		));
+
+		new tool(this.streamDeck, buttonId, buttonData.options);
 	}
 }
 
